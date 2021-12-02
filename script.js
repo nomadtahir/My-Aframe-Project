087e00b9c26d51ea09c0297dea6ec29b6568add6
Tahir Civan <69795597+nomadtahir@users.noreply.github.com>
2021-12-02 14:48:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var scene = document.querySelector('a-scene').object3D
+var sceneEl = document.querySelector('a-scene').object3D;
+var entityEl = document.querySelector('a-entity').object3D;
+var scene = sceneEl
 var loader = new THREE.GLTFLoader();
 const pickableObjects = []
 const lod = new THREE.LOD();
-
 loader.load('https://cdn.glitch.me/00f2d644-93e9-43db-b8e5-cac6ced4b897%2FtestNew4LOD.glb?', function (gltf) {
-    var model = gltf.scene;
-    gltf.scene.scale.set(0.01, 0.01, 0.01)
-
+    var model = gltf.scene
     model.traverse(function (o) {
         if (o instanceof THREE.Mesh) {
             pickableObjects.push(o)
@@ -24,11 +23,10 @@ loader.load('https://cdn.glitch.me/00f2d644-93e9-43db-b8e5-cac6ced4b897%2FtestNe
     lod.addLevel(theResult0, 30)
     lod.addLevel(theResult1, 60)
     lod.addLevel(theResult2, 80)
-    lod.scale.set(0.1, 0.1, 0.1)
+    lod.scale.set(0.01, 0.01, 0.01)
     lod.rotation.set(0, -1.5, 1.555)
-    lod.position.set(-7, 0, 0)
+    lod.position.set(-0.7, 1, -5)
     scene.add(lod)
-
     // scene.add(gltf.scene);
 
 });
